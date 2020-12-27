2127a0b691f79e89d9f359be935694aa257df1e2
janolehoffmann <51462206+janolehoffmann@users.noreply.github.com>
2020-12-27 14:00:54 +0100

    add ^ as power operator

--- eval.py
+++ eval.py
@@ -1,20 +1,23 @@
 import math
 
 def evaluate(input):
+    input = __replace_functions(input)
     try:
-        input = input.replace("sqrt", "math.sqrt")
-        input = input.replace("pow", "math.pow")
-        input = input.replace("log", "math.log")
-        input = input.replace("factorial", "math.factorial")
-
-        input = input.replace("sin", "math.sin")
-        input = input.replace("cos", "math.cos")
-        input = input.replace("tan", "math.tan")
-
-        input = input.replace("pi", "math.pi")
-        input = input.replace("e", "math.e")
-
         x = eval(input)
         return x
     except:
         return None
+
+
+def __replace_functions(input):
+    input = input.replace("sqrt", "math.sqrt")
+    input = input.replace("pow", "math.pow")
+    input = input.replace("^", "**")
+    input = input.replace("log", "math.log")
+    input = input.replace("factorial", "math.factorial")
+    input = input.replace("sin", "math.sin")
+    input = input.replace("cos", "math.cos")
+    input = input.replace("tan", "math.tan")
+    input = input.replace("pi", "math.pi")
+    input = input.replace("e", "math.e")
+    return input
